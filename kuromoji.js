0fef0edbbc53c89f60a91292b1d521a30cb17ee1
Onohara <onohara@nmedia.co.jp>
2024-03-03 02:42:04 +0900

    テスト的に一旦変更

--- kuromoji.js
+++ kuromoji.js
@@ -1,11 +1,18 @@
-import { kuromoji as _kuromoji } from "./src/kuromoji.js";
+import { TokenizerBuilder } from "./src/TokenizerBuilder.js";
+import { DictionaryBuilder } from "./src/dict/builder/DictionaryBuilder.js";
 
 const DIC_URL = "https://code4fukui.github.io/kuromoji-es/dict/";
 
 export const kuromoji = {
+  builder: function (option) {
+      return new TokenizerBuilder(option);
+  },
+  dictionaryBuilder: function () {
+      return new DictionaryBuilder();
+  }
   createTokenizer: async () => {
     return new Promise((resolve, reject) => {
-      _kuromoji.builder({ dicPath: DIC_URL }).build((err, tokenizer) => {
+      kuromoji.builder({ dicPath: DIC_URL }).build((err, tokenizer) => {
         if (err) {
           reject(err);
           return;
@@ -15,3 +22,5 @@ export const kuromoji = {
     });
   }
 };
+
+
